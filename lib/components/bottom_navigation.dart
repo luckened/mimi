6f34047eb43658fa4dcad4bcce633611102dc0ab
Lucca Miranda <lucca.silva@quintoandar.com.br>
2022-07-25 23:34:58 -0300

    feat: bottom navigation

--- lib/components/bottom_navigation.dart
+++ lib/components/bottom_navigation.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/material.dart';
 
-class BottomNavigation extends StatelessWidget {
-  const BottomNavigation({Key? key}) : super(key: key);
+class CustomBottomNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final void Function(int)? onTap;
+
+  const CustomBottomNavigationBar(
+      {Key? key, required this.currentIndex, required this.onTap})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    const labels  = [];
     return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
+          icon: Icon(Icons.medication_rounded),
+          label: 'Questionnaires',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.business),
-          label: 'Home',
+          icon: Icon(Icons.newspaper_rounded),
+          label: 'News',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.school),
-          label: 'Home',
+          icon: Icon(Icons.radar),
+          label: 'Contact Tracing',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.info_rounded),
+          label: 'Info',
         ),
       ],
+      selectedItemColor: Colors.amber,
+      currentIndex: currentIndex,
+      onTap: onTap,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,70 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'components/bottom_navigation.dart';
 
-
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Mimi';
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        bottomNavigationBar: const BottomNavigation(),
-        appBar: AppBar(
-          title: const Text('Mimi'),
-        ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Questionnaires',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: AAAA',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mimi'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
