4ad3945c69f3d79a6fff1d13d66ffc47ce713c43
Lucca Miranda <lucca.silva@quintoandar.com.br>
2022-08-03 23:16:22 -0300

    chore: remove unused widget

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,6 @@ class MyStatefulWidget extends StatefulWidget {
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
     QuestionnairesPage(),
     NewsPage(),
